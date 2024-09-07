e271a109629d836c132cf4d07c5838d0edc62bb6
mad-cost <mad_scientist1998@naver.com>
2024-09-07 17:21:42 +0900

    init

---
+++ object/src/test/java/com/_P/eureka/client/object/ObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com._P.eureka.client.object;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
