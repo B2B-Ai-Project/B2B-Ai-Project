e3319595beb37517a608d114863e5a9821fcee15
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-09-19 11:49:57 +0900

    #34- feat: Ai Application 개발 완료 (#35)
    * #34- feat: Ai Application 개발 완료

---
+++ ai/src/main/java/com/_P/eureka/client/ai/entity/GeminiRepository.java
@@ -0,0 +1,7 @@
+package com._P.eureka.client.ai.entity;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GeminiRepository extends JpaRepository<Gemini, Long> {
+}
