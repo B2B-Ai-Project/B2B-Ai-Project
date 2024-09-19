20d806d06a18f606263b82bed5c02c33ac9b7e02
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-09-19 11:41:25 +0900

    #18-refactor: Product 수량 입력 삭제 (#33)
    * #18- 생명 주기가 같은 3개의 객체 Create 완료
    
    * #18- Order 완성
    
    * #18- merge Complete / Order Complete
    
    * #18- DeliveryAPI 완성
    
    * #18- refactor: Product 수량 입력 삭제

---
+++ delivery/src/main/java/com/_P/eureka/client/delivery/presentaion/controller/DeliveryRecordController.java
@@ -0,0 +1,15 @@
+package com._P.eureka.client.delivery.presentaion.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/deliveryRecord")
+@RequiredArgsConstructor
+public class DeliveryRecordController {
+
+
+
+
+}
